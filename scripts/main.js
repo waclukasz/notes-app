e1f67657230457ef9efe6882b4407dfefa208d94
Łukasz Wac <69303987+wac-lukasz@users.noreply.github.com>
2020-11-08 11:04:37 +0100

    starts building validation

--- scripts/main.js
+++ scripts/main.js
@@ -12,15 +12,66 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const noteTitle = document.getElementById('noteTitle');
     const noteContent = document.getElementById('noteContent');
 
-    // Single Note Model
-    const newNote = {
-      title: noteTitle.value,
-      content: noteContent.value,
-      date: Date.now()
-    }
+    // const isFieldsValid = validateFields(noteTitle);
 
-    notesList.push(newNote);
-    console.log(notesList);
+    console.log(validateNoteContent(noteContent.value));
+
+    // if (isFieldsValid) {
+
+    // }
+
+    // if (noteContent.value && (noteTitle.value && noteTitle.value.length <= 20)) {
+    //     noteTitle.style.borderColor = '#394867';
+    //     // Single Note Model
+    //     const newNote = {
+    //       title: noteTitle.value,
+    //       content: noteContent.value,
+    //       date: Date.now()
+    //     }
+
+    //     notesList.push(newNote);
+
+    //     noteTitle.value = '';
+    //     noteContent.value = '';
+
+    //     toggleModal();
+    // } else {
+    //   generateError({
+    //     contentError: 'Field is required',
+    //     titleError: 'Field is required'
+    //   }, [noteTitle, noteContent])
+    // }
+  }
+
+  const validateFields = () => {
+
+  }
+
+  const validateNoteTitle = (title) => {
+    if (title) {
+      if (title.length <= 20) {
+        return true;
+      } else {
+        return 'Field should have max 20 chars.'
+      }
+    } else {
+      return 'Field is required';
+    }
+  }
+
+  const validateNoteContent = (content) => {
+    return !content.trim() && 'Field is requierd'
+  }
+
+  const generateError = (fields, inputs) => {
+    Object.keys(fields).forEach((key) => {
+      const errorField = document.getElementById(key);
+      errorField.innerText = fields[key];
+    })
+
+    inputs.forEach((input) => {
+      input.style.borderColor = 'red'
+    })
   }
 
 
